390bc60576488902a91f33ae2a7281899387f7ec
iamquoz <46261678+iamquoz@users.noreply.github.com>
2021-03-18 21:53:27 +0400

    still doesn't *quite* work with phones

--- client/toolbar.js
+++ client/toolbar.js
@@ -1,9 +1,4 @@
-var app = angular.module('platform',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'ngAnimate']);
-
-  
-app.controller('mainToolbar', function($scope){
-
-});
+var app = angular.module('platform',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'ngAnimate', 'ngGrid']);
 
 app.controller('mainSidebar', function ($scope, $timeout, $mdSidenav) {
     $scope.toggleLeft  = function () {
@@ -20,3 +15,32 @@ app.controller('mainSidebar', function ($scope, $timeout, $mdSidenav) {
       $mdSidenav('left').close();
     };
   });
+  
+  app.controller("mainDiary", function($scope, $http){
+    $http.get("./data.json")
+    .then(function (file) {
+      	$scope.teacher = file.data.teacher;
+      	$scope.group = file.data.group;
+		$scope.students = file.data.students;
+    }
+    ,function (data) {
+        console.log("there was an error");
+    });
+});
+
+app.controller('tableCtrl', function($scope, $http){
+	$http.get("./data.json").then(function(file) {
+		$scope.students = file.data.students;
+	});
+	$scope.gridOptions = {
+		data: 'students',
+		enableRowSelection: false,
+    rowHeight: 60,
+    headerRowHeight: 60,
+		columnDefs:[
+			{displayName:"ФИО", field: "stName", width: "65%"},
+			{displayName:"Баллы", field: "stScore", width: "35%"}
+		]
+	}	
+		
+});
